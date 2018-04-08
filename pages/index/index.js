ed810dbcc897e6a2b7a84093228ae3b241974f1c
Limengbo <15600099705@163.com>
2018-04-08 17:33:33 +0800

    :tada:图片上传和预览

---
+++ pages/index/index.js
@@ -0,0 +1,144 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tempFilePaths: []
+  },
+  /**
+   * 上传图片方法
+   */
+  upload: function () {
+    let that = this;
+    wx.chooseImage({
+      count: 9, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: res => {
+        wx.showToast({
+          title: '正在上传...',
+          icon: 'loading',
+          mask: true,
+          duration: 1000
+        })  
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        let tempFilePaths = res.tempFilePaths;
+
+        that.setData({
+          tempFilePaths: tempFilePaths
+        })
+        /**
+         * 上传完成后把文件上传到服务器
+         */
+        var count = 0;
+        for (var i = 0, h = tempFilePaths.length; i < h; i++) {
+          //上传文件
+        /*  wx.uploadFile({
+            url: HOST + '地址路径',
+            filePath: tempFilePaths[i],
+            name: 'uploadfile_ant',
+            header: {
+              "Content-Type": "multipart/form-data"
+            },
+            success: function (res) {
+              count++;
+              //如果是最后一张,则隐藏等待中  
+              if (count == tempFilePaths.length) {
+                wx.hideToast();
+              }
+            },
+            fail: function (res) {
+              wx.hideToast();
+              wx.showModal({
+                title: '错误提示',
+                content: '上传图片失败',
+                showCancel: false,
+                success: function (res) { }
+              })
+            }
+          });*/
+        }  
+        
+      }
+    })
+  },
+  /**
+   * 预览图片方法
+   */
+  listenerButtonPreviewImage: function (e) {
+    let index = e.target.dataset.index;
+    let that = this;
+    wx.previewImage({
+      current: that.data.tempFilePaths[index],
+      urls: that.data.tempFilePaths,
+      //这根本就不走
+      success: function (res) {
+        //console.log(res);
+      },
+      //也根本不走
+      fail: function () {
+        //console.log('fail')
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
